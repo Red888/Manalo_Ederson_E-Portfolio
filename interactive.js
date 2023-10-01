b2af0e352217ede24c2e92feeedb4d82e0f7bc22
Red888 <114731391+Red888@users.noreply.github.com>
2023-10-01 18:44:20 +0800

    Add files via upload

--- interactive.js
+++ interactive.js
@@ -12,3 +12,27 @@ const GenerateQuote = async () =>{
     document.getElementById("author").innerHTML="- "+auth;
  }
  GenerateQuote();
+
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+    const triggerButtom = window.innerHeight / 5  * 4;
+
+    boxes.forEach((box) => {
+        const boxTop = box.getBoundingClientRect().top;
+
+        if(boxTop < triggerButtom) { 
+            box.classList.add('showup');
+        } else {
+            box.classList.remove('showup');
+        }
+    });
+}
+  
+  
+  
+  
