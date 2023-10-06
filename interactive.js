e123a0546f1018ef67aff82887c825e099523d78
Red888 <114731391+Red888@users.noreply.github.com>
2023-10-06 19:39:14 +0800

    Add files via upload

--- interactive.js
+++ interactive.js
@@ -32,7 +32,22 @@ function checkBoxes() {
         }
     });
 }
-  
+
+//scroll effect
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddemElements = document.querySelectorAll('.hideElements');
+hiddemElements.forEach((el) => observer.observe(el));  
   
   
   
